b3d52e47edb701d1e31d18cd2ee0810fe0000a94
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-05-06 11:08:37 +0700

    enable click enter

--- src/comps/Input/index.js
+++ src/comps/Input/index.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './Input.module.scss';
@@ -12,10 +12,17 @@ export default function Input({
   buttonText = 'Add',
 }) {
   const [state, dispatch] = useStore();
+  const [taskDesc, setTaskDesc] = useState('');
   const inputRef = useRef();
 
-  function handleAdd(e) {
+  function handleAdd() {
     dispatch(cmds.addTask(inputRef.current.value));
+    setTaskDesc('');
+    inputRef.current.focus();
+  }
+
+  function handleKey(e) {
+    if (e.key == 'Enter') handleAdd();
   }
 
   return (
@@ -23,8 +30,11 @@ export default function Input({
       <input
         ref={inputRef}
         className={cx('input')}
+        value={taskDesc}
         placeholder={placeholder}
         spellCheck={false}
+        onKeyUp={handleKey}
+        onChange={(e) => setTaskDesc(e.target.value)}
       />
       <button className={cx('btn')} onClick={handleAdd}>
         {buttonText}
