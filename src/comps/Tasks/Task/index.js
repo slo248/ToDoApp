3491cb8124fabef519a4398b9e086ee3ec8e3d6f
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-05-07 12:11:07 +0700

    add edit feat

--- src/comps/Tasks/Task/index.js
+++ src/comps/Tasks/Task/index.js
@@ -7,6 +7,7 @@ import cmds from '~/store/commands';
 import styles from './Task.module.scss';
 import { TaskStatus } from '~/store/reducer';
 import StatusBox from '~/comps/StatusBox';
+import { useState, useRef, useEffect } from 'react';
 
 const cx = classNames.bind(styles);
 
@@ -14,7 +15,16 @@ export default function Task({ taskId }) {
   const [state, dispatch] = useStore();
   const task = state.tasks[taskId];
 
+  const [inp, setInp] = useState(task.desc);
+  const inputRef = useRef();
+
+  useEffect(() => inputRef?.current?.focus());
+
   function handleRemove() {
+    if (state.editTaskId === taskId) {
+      alert(`Task "${task.desc}" is being edited and cannot be deleted!`);
+      return;
+    }
     dispatch(cmds.delTask(taskId));
   }
 
@@ -24,17 +34,50 @@ export default function Task({ taskId }) {
     else dispatch(cmds.updTaskStatus(taskId, TaskStatus.DONE));
   }
 
+  function handleEdit() {
+    if (task.status === TaskStatus.DONE) {
+      alert(`Task "${task.desc}" is done and cannot be edited!`);
+      return;
+    }
+    dispatch(cmds.setEditTask(taskId));
+  }
+
+  function handleDoneEdit() {
+    dispatch(cmds.updTaskDesc(taskId, inp.trim()));
+    dispatch(cmds.setEditTask());
+  }
+
+  function handleKeyUp(e) {
+    if (e.key === 'Enter' || e.key === 'Escape') handleDoneEdit();
+  }
+
   return (
-    <div className={cx('wrapper')}>
-      <p
-        className={cx('desc', {
-          [cx('done')]: TaskStatus.getBool(task.status),
-        })}
-      >
-        {task.desc}
-      </p>
+    <div className={cx('wrapper')} onDoubleClick={handleEdit}>
+      {state.editTaskId === taskId ? (
+        <div className={cx('edit-wrapper')}>
+          <input
+            ref={inputRef}
+            className={cx('input')}
+            value={inp}
+            onChange={(e) => setInp(e.target.value)}
+            onKeyUp={handleKeyUp}
+            onBlur={handleDoneEdit}
+          />
+          <button className={cx('btn')} onClick={handleDoneEdit}>
+            Done
+          </button>
+        </div>
+      ) : (
+        <p
+          className={cx('desc', {
+            [cx('done')]: TaskStatus.getBool(task.status),
+          })}
+        >
+          {task.desc}
+        </p>
+      )}
       <div className={cx('btns')}>
-        <button className={cx('btn', 'btn-edit')}>
+        <button className={cx('btn', 'btn-edit')} onClick={handleEdit}>
           <FontAwesomeIcon icon={faPenToSquare} />
         </button>
         <StatusBox
